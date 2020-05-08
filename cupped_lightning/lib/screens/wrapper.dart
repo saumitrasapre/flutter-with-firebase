0a3aaea45cbb8409125408d292f43607cdc5dbbb
Saumitra Sapre <saumitrasapre69@gmail.com>
2020-05-08 19:35:51 +0530

    Basic App Skeleton developed

---
+++ cupped_lightning/lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:cupped_lightning/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    //Return either Home or Authenticate widgets
+    
+    return Home();
+  }
+}
